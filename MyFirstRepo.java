9ccc285c2195d3c25ab663c48d5684759b88c9fd
EnterGenericNameHere <25-06316@g.batstate-u.edu.ph>
2026-08-28 09:21:57 +0800

    Hello World i guess

---
+++ MyFirstRepo.java
@@ -0,0 +1,11 @@
+package IT1206_DeGuzmanKLGG_OOP1_Activities;
+
+/**
+ * MyFirstRepo
+ */
+public class MyFirstRepo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
